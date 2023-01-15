ebeea1ca629c35d91458d2d6977c2f54e0e4c5c2
FOD <fionnuala.k.odonnell@gmail.com>
2023-01-15 22:12:18 +0000

    added code to hide the content from the start of the quiz and show the first question

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,31 +1,82 @@
 let startButton = document.querySelector("#start");
-let choicesEl = document.querySelector("#choices")
+let choicesEl = document.querySelector("#choices");
+let timer = document.querySelector("#time");
 
-
-// put all the questions within one object array
-// structure question so you have an array of object this 
-
-
-
+// this is where the data for the quiz is stored
 let questions = [{question: "The conditions in and if / else statement is enclosed within __________ .", answers:[ "1. quotes", "2. curly brackets", "3. parentheses", "4. square brackets"], correctAnswer:3 },
 
 {question:"Array in JavaScript can be used to store ___________ .", answers:[ "1. numbers and strings", "2. other arrays ", "3. booleans", "4. all of the above"], correctAnswer:3 },
 {question:"A very useful tool used during development and debugging for printing content to the debugger is:", answers:[ "1. JavaScript", "2. Terminal / Bash", "3. for loops", "4. console.log"], correctAnswer:3 },
 {question:"Commonly used data types DO NOT include:", answers:[ "1. strings", "2. booleans",  "3. alerts", "4. number"], correctAnswer:3 },
 
-
 ]
+/*
+// this function adds the class .hide to the start quiz element
+
+function hideStart(){
+    let element = document.querySelector(".start");
+    element.classList.add(".hide")
+}
+
+// this function removes the .hide function from the question
+
+function showQuestions(){
+    let element = document.querySelector("#questions");
+    element.classList.remove(".hide");
+}
+*/
 
 let currentQuestion =0;
 
+//this code loops through the questions
 for (let index = 0; index < questions.length; index++) {
 
-
-    
     
 }
 
+document.querySelector("#question-title").textContent = questions[0].question
 
+
+// when the start button is clicked the start screen div is hidden 
+// the timer starts
+// the first question is made visible
+
+startButton.addEventListener("click", function (){
+        console.log("click");
+       let showQuestions = document.querySelector("#questions");
+       showQuestions.classList.remove("hide");
+
+
+
+       let hideStart = document.querySelector(".start");
+       hideStart.classList.add("hide");
+
+});
+
+
+   
+
+
+
+/*todoList.addEventListener("click", function (event) {
+
+
+    if (event.target.matches("button")) {
+  
+      let removeIndex = event.target.getAttribute("data-index");
+  
+  
+      todos.splice(removeIndex, 1);
+      localStorage.setItem("todos", JSON.stringify(todos));
+  
+      renderTodos();
+  
+  
+    }
+  
+  
+  
+  })
 
 
 
